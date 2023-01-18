0837c5d2344e5ab7ce632dfb88fdec302aff3c10
Thiago Ferreira Rodrigues <thiago.ferreirarod22@gmail.com>
2023-01-18 20:34:46 -0300

    Configurando modulo de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
+import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'mural',
+    pathMatch: 'full',
+  },
+  {
+    path: 'criar-pensamento',
+    component: CriarPensamentoComponent,
+  },
+  {
+    path: 'mural',
+    component: ListarPensamentoComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
