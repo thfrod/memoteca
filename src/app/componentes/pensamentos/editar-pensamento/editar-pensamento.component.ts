31d711f05da4a327a441c9c67cc63ded58258fac
Thiago Ferreira Rodrigues <thiago.ferreirarod22@gmail.com>
2023-01-18 23:31:30 -0300

    Criando componente editar-componente

---
+++ src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
+
+@Component({
+  selector: 'app-editar-pensamento',
+  templateUrl: './editar-pensamento.component.html',
+  styleUrls: ['./editar-pensamento.component.css'],
+})
+export class EditarPensamentoComponent {
+  pensamento: Pensamento = {
+    id: 0,
+    conteudo: '',
+    autoria: '',
+    modelo: '',
+  };
+
+  constructor(
+    private service: PensamentoService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.service
+      .buscarPorId(parseInt(id!))
+      .subscribe((pensamento) => (this.pensamento = pensamento));
+  }
+  editarPensamento() {
+    this.service
+      .editar(this.pensamento)
+      .subscribe(() => this.router.navigate(['/mural']));
+  }
+
+  cancelar() {
+    this.router.navigate(['/mural']);
+  }
+}
